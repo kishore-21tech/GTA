43196cb6ef20e96bc934e88988b799b86aea5c1b
kishore-21tech <kishoreyuvaraj33@gmail.com>
2025-07-31 12:15:49 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        for(int i =5;i>0;i--){
+            for(int j=i;j>0;i--){
+                System.out.println("*");
+            }
+        }
     }
 }
